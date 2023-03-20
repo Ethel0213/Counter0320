f8d9142444db166377dfdd2a32367a8e6637d6ef
spencer <o1103025@gm.pu.edu.tw>
2023-03-20 09:52:36 +0800

    第一次 commit

---
+++ app/src/main/java/tw/pu/edu/gm/o1103025/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.pu.edu.gm.o1103025
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
